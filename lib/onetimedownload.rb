08645fee960cb11c8a19a5a4a832d7d76603bea8
Les Aker <me@lesaker.org>
2014-07-08 21:48:52 -0400

    add module spec

--- lib/onetimedownload.rb
+++ lib/onetimedownload.rb
@@ -1,2 +1,16 @@
 ##
 # Simple tool for serving a file to a single recipient
+module OneTimeDownload
+  class << self
+    ##
+    # Insert a helper .new() method for creating a new Server object
+
+    def new(*args)
+      self::Server.new(*args)
+    end
+  end
+end
+
+require 'onetimedownload/file'
+require 'onetimedownload/server'
+require 'onetimedownload/version'
--- onetimedownload.gemspec
+++ onetimedownload.gemspec
@@ -14,6 +14,8 @@ Gem::Specification.new do |s|
   s.test_files  = `git ls-files spec/*`.split
   s.executables = ['otd']
 
+  s.add_dependency 'mercenary', '~> 0.3.3'
+
   s.add_development_dependency 'rubocop', '~> 0.24.0'
   s.add_development_dependency 'rake', '~> 10.3.0'
   s.add_development_dependency 'coveralls', '~> 0.7.0'
